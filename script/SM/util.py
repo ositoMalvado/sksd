f7e82019689362ae05e000d4f15533db92d2b07f
ositoMalvado <mordecaaii@gmail.com>
2025-03-21 16:22:22 -0300

    yesss

--- script/SM/util.py
+++ script/SM/util.py
@@ -89,7 +89,7 @@ def storage(line):
 def delete_everything(line):    
     main_output = widgets.Output()
     
-    ask = widgets.Label("Delete?")
+    ask = widgets.Label("Eliminar?")
     ask.add_class("del")
 
     yes = widgets.Button(description="Si")
